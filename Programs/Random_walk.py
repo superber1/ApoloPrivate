48c3eb84785dac55158ec58dc80d9ab011d91277
Apolo Gomes <apolo.sandinha@tecnico.ulisboa.pt>
2023-12-03 17:04:30 +0000

    Acabado

--- Programs/Random_walk.py
+++ Programs/Random_walk.py
@@ -3,8 +3,9 @@ import random
 import matplotlib.pyplot as plt
 
 class Particle:
-    def __init__(self, position: list) -> None:
+    def __init__(self, position: list, num:int) -> None:
         self._position = position
+        self._num = num
 
     def get_position(self):
         return self._position
@@ -15,18 +16,22 @@ class Particle:
     def travel(self, path:int):
         self._position.append(self._position[len(self._position) - 1] + path)
 
+    def get_num(self):
+        return self._num
 
-def new_particle(pos):
-    return Particle(pos)
+
+def new_particle(pos, i):
+    return Particle(pos, i)
 
 
 def random_walk(num_steps, prob_right, num_particles):
     particles_list = []
     start_list = [0]
 
+
     i = 0
     while i < num_particles:
-        particles_list.append(new_particle(start_list))
+        particles_list.append(new_particle(start_list.copy(), i))
         i += 1
 
     for x in range(num_steps):
@@ -42,15 +47,13 @@ def random_walk(num_steps, prob_right, num_particles):
     for particle in particles_list:
         particle_paths.append(particle.get_position())
     
-    print(particle_paths)
-
     createplot(num_steps, particle_paths)
     
     return particle_paths
 
 def createplot(num_steps, particle_paths):
     time = [x for x in range(len(particle_paths[0]))]
-    # Build the pl o t with a l l the p a r t i c l e s
+    #Build the plot with all the particles
     for particle_path in particle_paths:
         plt.plot(particle_path, time)
 
@@ -59,8 +62,8 @@ def createplot(num_steps, particle_paths):
     plt.ylabel("Time")
     plt.show()
 
-num_steps = 5
+num_steps = 100
 prob_right = 0.5
-num_particles = 5
+num_particles = 15
 
 random_walk(num_steps, prob_right, num_particles)
